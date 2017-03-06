b3c04ec9e4c6ef0fc6fa4ef286e676a66e890799
Anna Chan <anna@firma8.com>
2017-03-05 21:23:16 -0500

    Solution for word count

---
+++ exercism/elixir/word-count/word_count.exs
@@ -0,0 +1,34 @@
+defmodule Words do
+  
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t) :: map
+  def count(sentence) do
+    sentence
+    |> format_sentence()
+    |> count(%{})
+  end
+
+  def count([], counts), do: counts
+
+  def count([head|tail], counts) do 
+    if Map.has_key?(counts, head) do
+      count(tail, counts |> Map.put(head, counts[head] + 1))
+    else
+      count(tail, counts |> Map.put(head, 1))
+    end
+  end
+
+  def format_sentence(sentence) do
+    sentence 
+    |> String.replace(~r/[^\w-]+/u, " ")
+    |> String.replace(~r/_/, " ")
+    |> String.downcase()
+    |> String.trim()
+    |> String.split(" ")
+  end
+
+end
